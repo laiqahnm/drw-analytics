33405bc74c58247c06ed1108035b8016ac436665
laiqahnm <154499391+laiqahnm@users.noreply.github.com>
2026-06-10 19:41:30 +0700

    Add additional CSS and JS files to Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,16 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/asosiasi-analisis.css',
+                'resources/css/asosiasi-hasil.css',
+                'resources/css/asosiasi-dashboard.css',
+                'resources/css/asosiasi-riwayat.css',
+                'resources/css/asosiasi-layout.css',
+                'resources/js/app.js',
+                'resources/js/asosiasi-analisis.js'
+            ],
             refresh: true,
         }),
     ],
